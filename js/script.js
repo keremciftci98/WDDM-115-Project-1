04101055a32c8f972c99980ac10c496ba8343035
Kerem Ciftci <ckerem@sabanciuniv.edu>
2024-03-13 17:35:57 -0400

    Create 2 functions to change page.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 
 document.getElementById("paragraph-1").style.visibility = "hidden";
 
-function ShowInfo() {
+function ShowInfo(index) {
+    console.log("selected index: " + index);
   if (document.getElementById("paragraph-1").style.visibility === "visible") {
     document.getElementById("paragraph-1").style.visibility = "hidden";
   } else {
     document.getElementById("paragraph-1").style.visibility = "visible";
   }
 }
+
+
+function GoToTrailerPage() {
+    document.location.href = "/html/trailerpage.html";
+  }
+
+  function BackButton() {
+    document.location.href = "/html/index2.html";
+  }

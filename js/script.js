ee8feae1170816fd366e468d6908e83669fa543c
Kerem Ciftci <ckerem@sabanciuniv.edu>
2024-03-13 19:00:43 -0400

    Implemented paragraph to be displayed on info button click.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
-document.getElementById("paragraph-1").style.visibility = "hidden";
-
-function ShowInfo(index) {
+function SelectIndex(index) {
   console.log("selected index: " + index);
-  if (document.getElementById("paragraph-1").style.visibility === "visible") {
-    document.getElementById("paragraph-1").style.visibility = "hidden";
-  } else {
-    document.getElementById("paragraph-1").style.visibility = "visible";
-  }
-  selectedIndex = index;
-  localStorage.setItem("selectedIndex", selectedIndex);
+  idx = index;
+  localStorage.setItem("selectedIndex", index);
 }
 
-let selectedIndex;
+function ShowInfo() {
+  var selectedIndex = localStorage.getItem("selectedIndex");
+  console.log("ShowInfo index: " + selectedIndex);
+
+  if (selectedIndex == 1) {
+    document.getElementById("oppenheimer.des").style.display = "block";
+  } else if (selectedIndex == 2) {
+    document.getElementById("poorthings.des").style.display = "block";
+  } else if (selectedIndex == 3) {
+    document.getElementById("barbie.des").style.display = "block";
+  } else if (selectedIndex == 4) {
+    document.getElementById("theholdovers.des").style.display = "block";
+  } else if (selectedIndex == 5) {
+    document.getElementById("killersoftheflowermoon.des").style.display = "block";
+  }
+}
 
 function GoToTrailerPage() {
   document.location.href = "/html/trailerpage.html";
@@ -26,4 +34,30 @@ function ChangeTrailer() {
 
   console.log("ChangeTrailer: ");
   console.log("ChangeTrailer: " + selectedIndex);
+
+  if (selectedIndex == 0) {
+    document.getElementById("movietrailer").src =
+      "https://www.youtube.com/watch?v=bK6ldnjE3Y0";
+    console.log("ChangeTrailer: ");
+  } else if (selectedIndex == 1) {
+    document.getElementById("movietrailer").src =
+      "https://www.youtube.com/watch?v=bK6ldnjE3Y0";
+    console.log("ChangeTrailer: 11");
+  } else if (selectedIndex == 2) {
+    document.getElementById("movietrailer").src =
+      "https://www.youtube.com/watch?v=RlbR5N6veqw";
+    console.log("ChangeTrailer: 22");
+  } else if (selectedIndex == 3) {
+    document.getElementById("movietrailer").src =
+      "https://www.youtube.com/watch?v=pBk4NYhWNMM";
+    console.log("ChangeTrailer: 33");
+  } else if (selectedIndex == 4) {
+    document.getElementById("movietrailer").src =
+      "https://www.youtube.com/watch?v=AhKLpJmHhIg";
+    console.log("ChangeTrailer: 44");
+  } else if (selectedIndex == 5) {
+    document.getElementById("movietrailer").src =
+      "https://www.youtube.com/watch?v=EG0si5bSd6I";
+    console.log("ChangeTrailer: 55");
+  }
 }
